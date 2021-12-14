3828c7a895eea655c263ffcf3e80227685a7cf19
Lorcan Odufuwa-Bolger <lorcanodufuwabolger@gmail.com>
2021-12-14 15:55:54 +0900

    Added method descriptions tested package methods for bug fixing

--- OcuPath/dataframer.py
+++ OcuPath/dataframer.py
@@ -18,24 +18,24 @@ class DataFramer():
         or via Google Drive
         '''
         if drive:
-            homepath = os.path.join('content', 'drive', 'MyDrive', 'Colab Notebooks', 'Ocular pathology', 'ODIR-5K')
+            self.datapath = os.path.join('content', 'drive', 'MyDrive', 'Colab Notebooks', 'Ocular pathology', 'ODIR-5K')
         else:
-            homepath = os.path.join('..', 'raw_data')
-        return homepath
+            self.datapath = os.path.join('..', 'raw_data')
+        return self.datapath
 
     def read_data(self, drive=False, file='full_df.csv'):
         '''
         Reads the csv file into a Pandas DataFrame and stores it as an attribute
         '''
-        self.path = self.set_data_path(drive)
-        self.data = pd.read_csv(os.path.join(self.path, file), index_col='ID')
+        self.set_data_path(drive)
+        self.data = pd.read_csv(os.path.join(self.datapath, file), index_col='ID')
         return self.data
 
     def get_human_df(self):
         '''
         Returns human readable dataframe of each eye, ready to be made 'model ready'
         '''
-        if self.data:
+        if any(self.data):
             right_data = self.data[RIGHT_INFO].rename(columns=RIGHT_MAPPER)
             right_data[['Eye']] = 'Right'
 
@@ -48,21 +48,25 @@ class DataFramer():
         return None
 
     def get_model_df(self):
-        if self.human_df:
+        '''
+        Returns dataframe with string inputs binary encoded
+        '''
+        if any(self.human_df):
             self.model_df = self.human_df.copy()
             self.model_df['Patient Sex'] = self.model_df['Patient Sex'].map(SEX_MAPPER)
             self.model_df['Eye'] = self.model_df['Eye'].map(EYE_MAPPER)
             return self.model_df.rename(columns=MODEL_MAPPER)
         return None
 
-    def get_key_list(self, frame, key_list=None):
+    def get_key_list(self, series, key_list=None):
         '''
         Parses through the Diagnostic Keywords Series to create an exhaustive
         list of all keyphrases, separated by commas
+        Ensure to pass in a Series, as a DataFrame will return only the column title
         '''
         if key_list == None:
             key_list = []
-        for eye in frame:
+        for eye in series:
             keywords = re.findall(r'([\w\-\s]+)', eye)
             for word in keywords:
                 key_list.append(word.strip())
@@ -75,7 +79,7 @@ class DataFramer():
         Uses the self.data attribute to extract the filenames for the left and
         right eye images for each patient
         '''
-        if self.data:
+        if any(self.data):
             left = self.data[['Left-Fundus']].values.squeeze()
             right = self.data[['Right-Fundus']].values.squeeze()
             return left, right
@@ -85,4 +89,4 @@ class DataFramer():
         '''
         Test function to ensure that the class has reloaded when running in Jupyter
         '''
-        print(TARGET_LIST[0])
+        print(TARGET_LIST[1])
